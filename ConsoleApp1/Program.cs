603b06c3a6d98c741094cc6464e61a979f9822e0
Khavrolev <khavrolev@gmail.com>
2021-01-22 14:02:32 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            (string Name, string[] Dishes) User;
+
+            Console.Write("Введите имя: ");
+            User.Name = Console.ReadLine();
+
+            User.Dishes = new string[5];
+
+            for (int i = 0; i < User.Dishes.Length; i++)
+            {
+                Console.Write("Введите любимое блюдо №{0}: ", i + 1);
+                User.Dishes[i] = Console.ReadLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
