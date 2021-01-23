7abd00711b1ecf104b86b0e0ae13622138e80783
Khavrolev <khavrolev@gmail.com>
2021-01-23 23:23:03 +0300

    Revert "Unit 5.1.2"
    This reverts commit e48262b1589d535d4d8fea7a4ffc47cc73c0b390.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,38 +2,70 @@
 
 class MainClass
 {
-    static int[] GetArrayFromConsole()
+
+    static string ShowColor()
     {
-        var result = new int[5];
+        Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+        var color = Console.ReadLine();
 
-        for (int i = 0; i < result.Length; i++)
+        switch (color)
         {
-            Console.WriteLine("Введите элемент массива номер {0}", i + 1);
-            result[i] = int.Parse(Console.ReadLine());
+            case "red":
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("Your color is red!");
+                break;
+
+            case "green":
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("Your color is green!");
+                break;
+            case "cyan":
+                Console.BackgroundColor = ConsoleColor.Cyan;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("Your color is cyan!");
+                break;
+            default:
+                Console.BackgroundColor = ConsoleColor.Yellow;
+                Console.ForegroundColor = ConsoleColor.Red;
+
+                Console.WriteLine("Your color is yellow!");
+                break;
         }
-
-        int temp;
-
-        for (int i = 0; i < result.Length; i++)
-        {
-            for (int j = i + 1; j < result.Length; j++)
-            {
-                if (result[i] > result[j])
-                {
-                    temp = result[i];
-                    result[i] = result[j];
-                    result[j] = temp;
-                }
-            }
-            Console.WriteLine(result[i]);
-        }
-
-        return result;
+        return color;
     }
 
     public static void Main(string[] args)
     {
-        int[] array = GetArrayFromConsole();
+
+        var (name, age) = ("Евгения", 27);
+
+        Console.WriteLine("Мое имя: {0}", name);
+        Console.WriteLine("Мой возраст: {0}", age);
+
+        Console.Write("Введите имя: ");
+        name = Console.ReadLine();
+        Console.Write("Введите возраст с цифрами: ");
+        age = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Ваше имя: {0}", name);
+        Console.WriteLine("Ваш возраст: {0}", age);
+
+        string[] favcolors = new string[3];
+        for (int i = 0; i < favcolors.Length; i++) {
+            favcolors[i] = ShowColor();
+        }
+
+        Console.WriteLine("Ваши любимые цвета:");
+
+        foreach (var color in favcolors)
+        {
+            Console.WriteLine(color);
+        }
 
         Console.ReadKey();
     }
