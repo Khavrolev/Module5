11b5b99c2389d322b6f1ab16a3442fc084cdf202
Sergey Khavrolev <khavrolev@gmail.com>
2021-01-25 22:20:51 +0300

    Unit 5.5.1 v.2

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -24,6 +24,7 @@ class MainClass
             modif = modif.Remove(0, 2);
         }
 
+        Console.BackgroundColor = (ConsoleColor)deep;
         Console.WriteLine("..." + modif);
 
         if (deep > 1)
